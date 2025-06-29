50b24c5ed95682c30932724bb11b0b80ac82f717
myothet <itlover9@gmail.com>
2025-06-29 22:59:05 +0630

    edited with static files

--- LittleLemon/LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/LittleLemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'LittleLemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],  # Adjust this path as needed],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,9 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'Restaurant/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print("BASE_DIR is:", BASE_DIR)
