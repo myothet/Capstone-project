1ecef5dc3b6ee61ec4886f314f14350e067546f1
myothet <itlover9@gmail.com>
2025-06-30 22:26:20 +0630

    added db tables

--- LittleLemon/LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/LittleLemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Restaurant',  # Ensure this app is included
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER':'lemonadmin',
+        'PASSWORD': 'lemon@12345',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+
+
     }
 }
 
--- LittleLemon/LittleLemon/Restaurant/models.py
+++ LittleLemon/LittleLemon/Restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
+class Booking(models.Model):
+    id=models.IntegerField(primary_key=True, verbose_name="Booking ID")
+    name=models.CharField(max_length=255, verbose_name="Customer Name")
+    no_of_guests=models.IntegerField(verbose_name="Number of Guests")
+    booking_date= models.DateField(verbose_name="Booking Date")
+    
+
+    def __str__(self):
+        return f"{self.name} - {self.booking_date}"
+    
+
+
+
+
+class Menu(models.Model):
+    id=models.IntegerField(primary_key=True, verbose_name="Menu Item ID")
+    title=models.CharField(max_length=255, verbose_name="Menu Item Title")
+    price=models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Price")
+    inventory=models.IntegerField()
+   
+
 # Create your models here.
