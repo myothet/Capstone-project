00a142249d3dcdbecb0d5989bf32e39d7c0fb5ce
myothettun <itlover9@gmail.com>
2025-07-06 22:03:11 +0630

    Update serializers.py by adding UserRegisterSerializer

--- LittleLemon/LittleLemon/Restaurant/serializers.py
+++ LittleLemon/LittleLemon/Restaurant/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models  import Menu,Booking
 from django.contrib.auth.models import User, Group
+from rest_framework.serializers import ModelSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,6 +10,18 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
+
+class UserRegistrationSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        customer_group, _ = Group.objects.get_or_create(name='Customer')
+        user.groups.add(customer_group)
+        return user
 
 class MenuItemSerializer(serializers.ModelSerializer):
 
